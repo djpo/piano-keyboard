d5046acf1a5d12274be2637874b8952322ab0bd1
Poppe <djpo@users.noreply.github.com>
2022-07-28 17:17:07 +0200

    differentiate black and white notes

--- components/key.tsx
+++ components/key.tsx
@@ -4,8 +4,10 @@ interface Props {
   midiNote: number,
 }
 
+const blackNotes = [22, 25, 27, 30, 32];
+
 const Key = ({ midiNote }: Props) => (
-  <div className={styles.key}>
+  <div className={blackNotes.includes(midiNote) ? styles.blackKey : styles.whiteKey}>
     {midiNote}
   </div>
 )
